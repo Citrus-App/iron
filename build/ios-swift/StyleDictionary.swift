e2cb03b26b649618fd1437d8fb01ae023fb9e52b
Jared Smith <jared@smithprocess.com>
2022-01-18 13:21:27 -0800

    track build

---
+++ build/ios-swift/StyleDictionary.swift
@@ -0,0 +1,130 @@
+
+//
+// StyleDictionary.swift
+//
+
+// Do not edit directly
+// Generated on Tue, 18 Jan 2022 21:19:50 GMT
+
+
+import UIKit
+
+public class StyleDictionary {
+    public static let globalFontFamiliesSans = SF Compact
+    public static let globalFontFamiliesSerif = IvyPresto Display
+    public static let globalFontSize0 = 20
+    public static let globalFontSize1 = 40
+    public static let globalFontSize2 = 60
+    public static let globalFontWeightsSans = Regular
+    public static let globalFontWeightsSerif = Regular
+    public static let globalHeader1 = [object Object]
+    public static let globalHeader2 = [object Object]
+    public static let globalLetterSpacing0 = 0
+    public static let globalLineHeights0 = 68
+    public static let globalLineHeights1 = 48
+    public static let globalLineHeights2 = 30
+    public static let globalMainbody = [object Object]
+    public static let globalParagraphSpacing0 = 0
+    public static let globalTextCaseNone = none
+    public static let globalTextDecorationNone = none
+    public static let globalTextHeadline = #1d1d1f
+    public static let lightBorderAccent = #183f7b
+    public static let lightBorderCritical = #f1465a
+    public static let lightBorderDefault = #cccfd9
+    public static let lightBorderDivider = #edeff5
+    public static let lightButtonDefault = #3165b3
+    public static let lightButtonHover = #22549f
+    public static let lightButtonOutlineDefault = #edeff5
+    public static let lightButtonOutlineHover = #dfe2eb
+    public static let lightButtonOutlinePressed = #cccfd9
+    public static let lightButtonPressed = #183f7b
+    public static let lightIconAccent = #183f7b
+    public static let lightIconCritical = #f1465a
+    public static let lightIconDefault = #313338
+    public static let lightIconInfo = #183f7b
+    public static let lightIconSuccess = #148565
+    public static let lightIconWarning = #c73f00
+    public static let lightIconWhite = #ffffff
+    public static let lightSurfaceAccent = #183f7b
+    public static let lightSurfaceAccentLight = #e5eefb
+    public static let lightSurfaceAlt = #f8f9fc
+    public static let lightSurfaceAltHover = #edeff5
+    public static let lightSurfaceCritical = #ffe5e7
+    public static let lightSurfaceDefault = #ffffff
+    public static let lightSurfaceDefaultHover = #f8f9fc
+    public static let lightSurfaceDialog = #ffffff
+    public static let lightSurfaceDialogHover = #f8f9fc
+    public static let lightSurfaceInfo = #e5eefb
+    public static let lightSurfaceOnAccent = #ffffff
+    public static let lightSurfaceOnAccentLight = #183f7b
+    public static let lightSurfaceSuccess = #daf6e5
+    public static let lightSurfaceTextfield = #dfe2eb
+    public static let lightSurfaceToast = #313338
+    public static let lightSurfaceToastError = #f1465a
+    public static let lightSurfaceWarning = #fff1cc
+    public static let lightSurfaceWash = #f8f9fc
+    public static let lightTextBody = #313338
+    public static let lightTextButton = #313338
+    public static let lightTextButtonOutline = #cccfd9
+    public static let lightTextCritical = #f1465a
+    public static let lightTextHeadline = #1d1d1f
+    public static let lightTextInfo = #183f7b
+    public static let lightTextLink = #183f7b
+    public static let lightTextSuccess = #148565
+    public static let lightTextWarning = #c73f00
+    public static let spectrumBlue000 = #f7f9fe
+    public static let spectrumBlue100 = #e5eefb
+    public static let spectrumBlue1000 = #0a1930
+    public static let spectrumBlue200 = #d1e0f6
+    public static let spectrumBlue300 = #a4c1ed
+    public static let spectrumBlue400 = #6697e1
+    public static let spectrumBlue500 = #4279cb
+    public static let spectrumBlue600 = #3165b3
+    public static let spectrumBlue700 = #22549f
+    public static let spectrumBlue800 = #183f7b
+    public static let spectrumBlue900 = #102a52
+    public static let spectrumGray000 = #ffffff
+    public static let spectrumGray100 = #f8f9fc
+    public static let spectrumGray1000 = #000000
+    public static let spectrumGray200 = #edeff5
+    public static let spectrumGray300 = #dfe2eb
+    public static let spectrumGray400 = #cccfd9
+    public static let spectrumGray500 = #a9adb8
+    public static let spectrumGray600 = #818691
+    public static let spectrumGray700 = #5b5f69
+    public static let spectrumGray800 = #313338
+    public static let spectrumGray900 = #1d1d1f
+    public static let spectrumGreen000 = #f3fcf9
+    public static let spectrumGreen100 = #daf6e5
+    public static let spectrumGreen1000 = #00291f
+    public static let spectrumGreen200 = #bdefd7
+    public static let spectrumGreen300 = #7be0b1
+    public static let spectrumGreen400 = #0dc994
+    public static let spectrumGreen500 = #09a57b
+    public static let spectrumGreen600 = #148565
+    public static let spectrumGreen700 = #07694d
+    public static let spectrumGreen800 = #094e3a
+    public static let spectrumGreen900 = #003d2e
+    public static let spectrumRed000 = #fff5f5
+    public static let spectrumRed100 = #ffe5e7
+    public static let spectrumRed1000 = #320109
+    public static let spectrumRed200 = #ffc2c6
+    public static let spectrumRed300 = #ff9ea5
+    public static let spectrumRed400 = #f96c73
+    public static let spectrumRed500 = #f1465a
+    public static let spectrumRed600 = #e32144
+    public static let spectrumRed700 = #be1332
+    public static let spectrumRed800 = #990620
+    public static let spectrumRed900 = #650113
+    public static let spectrumYellow000 = #fffbf0
+    public static let spectrumYellow100 = #fff1cc
+    public static let spectrumYellow1000 = #752100
+    public static let spectrumYellow200 = #ffe7a7
+    public static let spectrumYellow300 = #ffdc7b
+    public static let spectrumYellow400 = #ffd04f
+    public static let spectrumYellow500 = #ffc424
+    public static let spectrumYellow600 = #ffad1f
+    public static let spectrumYellow700 = #fa8900
+    public static let spectrumYellow800 = #e55c00
+    public static let spectrumYellow900 = #c73f00
+}
