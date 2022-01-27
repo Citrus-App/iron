47e84b39c3d93beb015dc0bed248a610131190f5
Jared Smith <jared@smithprocess.com>
2022-01-27 08:19:39 -0800

    holding

--- build/ios-swift/StyleDictionary.swift
+++ build/ios-swift/StyleDictionary.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 26 Jan 2022 22:01:29 GMT
+// Generated on Thu, 27 Jan 2022 15:33:46 GMT
 
 
 import UIKit
--- rollup.config.js
+++ rollup.config.js
@@ -2,52 +2,18 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
-// import dts from "rollup-plugin-dts";
-// import {terser} from 'rollup-plugin-terser';
-// import autoprefixer from 'autoprefixer';
+import {terser} from 'rollup-plugin-terser';
+import autoprefixer from 'autoprefixer';
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
-// const packageJson = require("./package.json");
+// import stylelint from "stylelint";
+// import postcssReporter from "postcss-reporter";
+// import postcssImport from "postcss-import";
+// import postcssNested from "postcss-nested";
+// import postcssPresetEnv from "postcss-preset-env";
 
-// export default [
-//   {
-//     // preserveModules: true,
-//     input: "src/index.ts",
-//     output: [
-//       {
-//         file: packageJson.main,
-//         format: "cjs",
-//         sourcemap: true,
-//       },
-//       {
-//         file: packageJson.module,
-//         format: "esm",
-//         sourcemap: true,
-//       },
-//     ],
-//     plugins: [
-//       peerDepsExternal(),
-//       resolve(),
-//       commonjs(),
-//       typescript({ tsconfig: "./tsconfig.json" }),
-//       postcss({
-//         extract: false,
-//         modules: true,
-//         plugins: [autoprefixer()],
-//       }),
-//       terser()
-//     ],
-//   },
-//   {
-//     input: "dist/esm/types/index.d.ts",
-//     output: [{ file: "dist/index.d.ts", format: "esm" }],
-//     plugins: [dts()],
-//     external: [/\.(css)$/],
-//   },
-// ];
-
-// rollup.config.js
-import babel from 'rollup-plugin-babel';
+const packageJson = require("./package.json");
+// import babel from 'rollup-plugin-babel';
 
 export default {
   input: "src/index.ts",
@@ -65,14 +31,23 @@ export default {
   plugins: [
     peerDepsExternal(),
     postcss({
+      plugins: [
+        autoprefixer()
+        // // stylelint(),
+        //   postcssImport({
+        //     addModulesDirectories: ["./build/tokens"],
+        //   }),
+        // postcssNested(),
+        // postcssReporter(),
+      ],
       extract: false,
       modules: true,
-      use: ['sass'],
     }),
-    babel({ exclude: 'node_modules/**' }),
+    // babel({ exclude: 'node_modules/**' }),
     resolve(),
     commonjs(),
     typescript({ tsconfig: "./tsconfig.json" }),
+    terser()
   ],
 
   external: ['react', 'react-dom'],
