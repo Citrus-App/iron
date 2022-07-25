9df48e1a2d72887b274874f2ffe170d8c5112fb9
Henry Fradley <fradleyhenry@gmail.com>
2022-07-24 17:33:10 -0700

    convert integerVals to type

--- src/utils/paddings/paddings.ts
+++ src/utils/paddings/paddings.ts
@@ -1,11 +1,11 @@
-const integerVals = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+type integerVals = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
 
 interface PaddingsTypes {
-  p: typeof integerVals
-  pb: typeof integerVals
-  pl: typeof integerVals
-  pr: typeof integerVals
-  pt: typeof integerVals
+  p: integerVals
+  pb: integerVals
+  pl: integerVals
+  pr: integerVals
+  pt: integerVals
 }
 
 export default PaddingsTypes
