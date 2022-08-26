b25d1a41d5a0b3cb69ceb50efa43700ca2d31254
Henry Fradley <fradleyhenry@gmail.com>
2022-08-25 21:16:13 -0700

    rollup

--- dist/types/atoms/Icon/Icon.d.ts
+++ dist/types/atoms/Icon/Icon.d.ts
@@ -1,5 +1,3 @@
 /// <reference types="react" />
-import { icons } from './defs';
 import { IconProps } from './types';
 export declare const Icon: ({ variant, toolTip, invert, ...props }: IconProps) => JSX.Element;
-export { icons };
